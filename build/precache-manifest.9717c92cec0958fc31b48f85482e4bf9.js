ced2539c94ff48bffd155913b11c64ec890a8080
leonmaass <leonard.maass@gmail.com>
2019-11-09 19:23:53 +0100

    Revert "deploy"

---
+++ build/precache-manifest.9717c92cec0958fc31b48f85482e4bf9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287a62306794bcd3b416e1cd149830f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8218a112922f1bf9f23",
+    "url": "/static/js/2.8ce9ced9.chunk.js"
+  },
+  {
+    "revision": "e1fb1978fe2a3be45088",
+    "url": "/static/js/main.6435e88e.chunk.js"
+  },
+  {
+    "revision": "984409dfc0344a9251ed",
+    "url": "/static/js/runtime-main.7319c8ac.js"
+  }
+]);
